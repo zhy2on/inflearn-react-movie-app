d0b4d216757ada764030e355bdcd0f13f7f44d05
zhy2on <dhwlgus5940@gmail.com>
2023-06-27 17:31:09 +0900

    feat: favorite icon 생성 - icon에 필요한 State 추가(Favorited, FavoriteNumber)

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,7 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Axios from 'axios';
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 function Favorite(props) {
+
+	const [FavoriteNumber, setFavoriteNumber] = useState(0);
+	const [Favorited, setFavorited] = useState(false);
 
 	useEffect(() => {
 
@@ -20,6 +24,7 @@ function Favorite(props) {
 			.then(res => {
 				if (res.data.success) {
 					console.log(res.data)
+					setFavoriteNumber(res.data.favoriteNumber)
 				} else {
 					alert('숫자 정보를 가져오는 데 실패 했습니다.')
 				}
@@ -29,6 +34,7 @@ function Favorite(props) {
 			.then(res => {
 				if (res.data.success) {
 					console.log(res.data)
+					setFavorited(res.data.favorited)
 			} else {
 				alert('정보를 가져오는 데 실패 했습니다.')
 			} 
@@ -37,7 +43,15 @@ function Favorite(props) {
 
 
 	return (
-		<button>Favorite</button>
+		<div>
+			<div>
+			{Favorited ? <AiFillHeart style={{ color: 'pink', fontSize: '40px'}} />
+				: <AiOutlineHeart style={{ color: 'pink', fontSize: '40px'}} />}
+			</div>
+			<div>
+				{FavoriteNumber} likes 
+			</div>
+		</div>
 	)
 }
 
