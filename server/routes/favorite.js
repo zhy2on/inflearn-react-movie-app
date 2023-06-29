d37e752a99990b3cc84fbc160519b1b33e70febb
zhy2on <dhwlgus5940@gmail.com>
2023-06-29 20:44:57 +0900

    chore: 형식 맞게 수정

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -13,7 +13,7 @@ router.post('/favoriteNumber', (req, res) => {
 
 router.post('/favorited', (req, res) => {
 	// 내가 이 영화를 Favorite 리스트에 넣었는지 정보를 가져오기
-	Favorite.find({ 'movieId': req.body.movieId, "userFrom": req.body.userFrom })
+	Favorite.find({ 'movieId': req.body.movieId, 'userFrom': req.body.userFrom })
 		.exec((err, info) => {
 			if (err) return res.status(400).send(err)
 
@@ -36,7 +36,7 @@ router.post('/addToFavorite', (req, res) => {
 })
 
 router.post('/removeFromFavorite', (req, res) => {
-	Favorite.findOneAndDelete({ movieId: req.body.movieId, userFrom: req.body.userFrom })
+	Favorite.findOneAndDelete({ 'movieId': req.body.movieId, 'userFrom': req.body.userFrom })
 		.exec((err, doc) => {
 			if (err) return res.status(400).send(err)
 			return res.status(200).json({ success: true, doc })
